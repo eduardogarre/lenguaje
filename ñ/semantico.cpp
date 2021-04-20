cabc79f6b7a8e38421a86a6219fc8c6b4dfb4c34
eduardogarre <eduardo.garre@gmail.com>
2021-04-20 13:51:34 +0200

    Divido análisis semántico en nombres y tipos

--- ñ/semantico.cpp
+++ ñ/semantico.cpp
@@ -6,7 +6,108 @@
 #include "semantico.hpp"
 #include "simbolo.hpp"
 
-std::map<std::string, Ñ::Símbolo> tablaSímbolos;
+Ñ::Resultado resuelveSímbolos(Ñ::Nodo* nodos, std::map<std::string, Ñ::Símbolo>* tablaSímbolos)
+{
+    Ñ::Resultado resultado;
+
+    if( nodos->ramas.size() > 0)
+    {
+        if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_LLAMA_FUNCIÓN)
+        {
+            Ñ::LlamaFunción* fn = (Ñ::LlamaFunción*)(nodos->ramas[0]);
+            
+            if( tablaSímbolos->count(fn->función) == 0 )
+            {
+                resultado.error("RESOLUCIÓN DE SÍMBOLOS :: " + fn->función + "() no está en la tabla de símbolos");
+                return resultado;
+            }
+            else
+            {
+                Ñ::Símbolo s = tablaSímbolos->at(fn->función);
+                if(!s.esFunciónEjecutable() && !s.esFunciónImplementada())
+                {
+                    resultado.error("RESOLUCIÓN DE SÍMBOLOS :: " + fn->función + "() está en la tabla de símbolos pero sin implementación");
+                    return resultado;
+                }
+            }
+
+            resultado.éxito();
+            return resultado;
+        }
+        else if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_DECLARA_VARIABLE)
+        {
+            auto declvar = nodos->ramas[0];
+            std::string nombre = ((Ñ::DeclaraVariable*)declvar)->variable;
+            if(tablaSímbolos->count(nombre) > 0 )
+            {
+                resultado.error("RESOLUCIÓN DE SÍMBOLOS :: El identificador \"" + nombre + "\" ya se había declarado previamente");
+                return resultado;
+            }
+
+            if( declvar->ramas.size() != 1)
+            {
+                resultado.error("RESOLUCIÓN DE SÍMBOLOS :: Árbol de la declaración de variable mal construido");
+                return resultado;
+            }
+
+            resultado.éxito();
+            return resultado;
+        }
+    }
+
+    resultado.error("RESOLUCIÓN DE SÍMBOLOS :: No reconozco el árbol de nodos");
+    return resultado;
+}
+
+Ñ::Resultado compruebaTipos(Ñ::Nodo* nodos, std::map<std::string, Ñ::Símbolo>* tablaSímbolos)
+{
+    Ñ::Resultado resultado;
+    
+    if( nodos->ramas.size() > 0)
+    {
+        if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_LLAMA_FUNCIÓN)
+        {
+            resultado.éxito();
+            return resultado;
+        }
+        else if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_DECLARA_VARIABLE)
+        {
+            auto declvar = nodos->ramas[0];
+            std::string nombre = ((Ñ::DeclaraVariable*)declvar)->variable;
+            if(declvar->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_TIPO)
+            {
+                auto tipo = (declvar->ramas[0]);
+                std::string cadenaTipo = ((Ñ::Tipo*)tipo)->tipo;
+
+                if( cadenaTipo == "bool" ||
+                    cadenaTipo == "ent" ||
+                    cadenaTipo == "nat" ||
+                    cadenaTipo == "real" ||
+                    cadenaTipo == "txt" )
+                {
+                    resultado.éxito();
+                    return resultado;
+                }
+                else
+                {
+                    resultado.error("COMPROBACIÓN DE TIPOS :: Tipo desconocido");
+                    return resultado;
+                }
+            }
+            else
+            {
+                resultado.error("COMPROBACIÓN DE TIPOS :: La declaración de variable no contiene tipo");
+                return resultado;
+            }
+
+            resultado.éxito();
+            return resultado;
+        }
+    }
+
+    resultado.error("COMPROBACIÓN DE TIPOS :: No reconozco el árbol de nodos");
+    return resultado;
+}
 
 Ñ::Resultado Ñ::analizaSemántica(Ñ::Nodo* nodos, std::map<std::string, Ñ::Símbolo>* tablaSímbolos)
 {
@@ -24,75 +125,19 @@ std::map<std::string, Ñ::Símbolo> tablaSímbolos;
         return resultado;
     }
 
-    if( nodos->ramas.size() == 1)
+    Ñ::Resultado rResuelveSímbolos = resuelveSímbolos(nodos, tablaSímbolos);
+    if(rResuelveSímbolos.error())
     {
-        if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_LLAMA_FUNCIÓN)
-        {
-            Ñ::LlamaFunción* fn = (Ñ::LlamaFunción*)(nodos->ramas[0]);
-            
-            if( tablaSímbolos->count(fn->función) == 0 )
-            {
-                resultado.error("SEMÁNTICO :: " + fn->función + "() no está en la tabla de símbolos");
-                return resultado;
-            }
-            else
-            {
-                Ñ::Símbolo s = tablaSímbolos->at(fn->función);
-                if(!s.esFunciónEjecutable() && !s.esFunciónImplementada())
-                {
-                    resultado.error("SEMÁNTICO :: " + fn->función + "() está en la tabla de símbolos pero sin implementación");
-                    return resultado;
-                }
-            }
-
-            resultado.éxito();
-            return resultado;
-        }
-        else if(nodos->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_DECLARA_VARIABLE)
-        {
-            auto declvar = nodos->ramas[0];
-            std::string nombre = ((Ñ::DeclaraVariable*)declvar)->variable;
-            if(tablaSímbolos->count(nombre) > 0 )
-            {
-                resultado.error("SEMÁNTICO :: El identificador \"" + nombre + "\" ya se había declarado previamente");
-            }
-
-            if( declvar->ramas.size() != 1)
-            {
-                resultado.error("SEMÁNTICO :: Árbol de la declaración de variable mal construido");
-                return resultado;
-            }
-            else if(declvar->ramas[0]->categoría == Ñ::CategoríaNodo::NODO_TIPO)
-            {
-                auto tipo = (declvar->ramas[0]);
-                std::string cadenaTipo = ((Ñ::Tipo*)tipo)->tipo;
-
-                if( cadenaTipo == "bool" ||
-                    cadenaTipo == "ent" ||
-                    cadenaTipo == "nat" ||
-                    cadenaTipo == "real" ||
-                    cadenaTipo == "txt" )
-                {
-                    resultado.éxito();
-                    return resultado;
-                }
-                else
-                {
-                    resultado.error("SEMÁNTICO :: Tipo desconocido");
-                    return resultado;
-                }
-            }
-            else
-            {
-                resultado.error("SEMÁNTICO :: La declaración de variable no contiene tipo");
-                return resultado;
-            }
-
-            resultado.éxito();
-            return resultado;
-        }
+        resultado.error("SEMÁNTICO :: " + rResuelveSímbolos.mensaje());
+        return resultado;
+    }
+    Ñ::Resultado rCompruebaTipos = compruebaTipos(nodos, tablaSímbolos);
+    if(rCompruebaTipos.error())
+    {
+        resultado.error("SEMÁNTICO :: " + rCompruebaTipos.mensaje());
+        return resultado;
     }
 
-    resultado.error("SEMÁNTICO :: No reconozco el árbol de nodos");
+    resultado.éxito();
     return resultado;
 }

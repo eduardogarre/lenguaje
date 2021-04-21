2fc8b68df2605f8354847168b8ba5be869e87914
eduardogarre <eduardo.garre@gmail.com>
2021-04-21 23:34:41 +0200

    Actualizo guiones de construcción

--- construye.ps1
+++ construye.ps1
@@ -1,23 +1,29 @@
-﻿rm obra -Force -Recurse -ErrorAction SilentlyContinue
+﻿rm build -Force -Recurse -ErrorAction SilentlyContinue
 rm proyecto -Force -Recurse -ErrorAction SilentlyContinue
-rm build -Force -Recurse -ErrorAction SilentlyContinue
+rm obra -Force -Recurse -ErrorAction SilentlyContinue
 
 mkdir obra
 mkdir proyecto
 
 cd obra
 cmake ..
-cmake --build .
+cmake --build . --parallel=4
 cd ..
 
-#rm obra -Force -Recurse -ErrorAction SilentlyContinue
+rm obra -Force -Recurse -ErrorAction SilentlyContinue
 
 mv ./proyecto/compilador.exe ./proyecto/ñ.exe
 
 echo ""
 echo ""
-echo "Ejecutando ñ"
+echo "Paso Pruebas"
 echo ""
 
 ./proyecto/pruebas.exe
-#./proyecto/ñ.exe
+
+echo ""
+echo ""
+echo "Ejecuto ñ"
+echo ""
+
+./proyecto/ñ.exe

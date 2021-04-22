9706a5d8a8a27c5e5c5af0be406d0728b5b91a15
eduardogarre <eduardo.garre@gmail.com>
2021-04-22 09:24:11 +0200

    Pongo pruebas de ejemplo

--- pruebas/pruebas.cpp
+++ pruebas/pruebas.cpp
@@ -1,11 +1,18 @@
 ﻿#include "gtest/gtest.h"
 #include "../ñ/nodo.hpp"
 
+/*
 TEST(PruebasNodos, DuplicaNodos)
 {
-    Ñ::Literal* lit1 = new Ñ::Literal();
-    Ñ::Literal* lit2;
-    lit1->dato = "hola";
-    lit2 = (Ñ::Literal*) Ñ::duplicaNodo((Ñ::Nodo*)lit1);
-    EXPECT_EQ(lit1->dato, lit2->dato);
+    Ñ::Literal* literal1 = new Ñ::Literal();
+    Ñ::Literal* literal2;
+    literal1->dato = "hola";
+    literal2 = (Ñ::Literal*) Ñ::duplicaNodo((Ñ::Nodo*)literal1);
+    EXPECT_EQ(literal1->dato, literal2->dato);
+}
+*/
+
+TEST(Prueba, Ejemplo)
+{
+    EXPECT_EQ(1, 1);
 }

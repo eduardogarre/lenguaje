f33dd3c11c267fa26ce47ce895049af4c22839e4
Eduardo Garre <eduardo.garre@gmail.com>
2021-09-19 12:14:34 +0200

    Separo biblioteca estándar

---
+++ biblioteca/bibñ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+extern "C" {
+    int inicia();
+    
+    void escribe(int numero)
+    {
+        std::cout << numero << std::endl;
+    }
+}
+
+int main() {
+    inicia();
+}

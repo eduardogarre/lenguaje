b88983faf9f6b01a6ad8f803b187eb285568270f
Eduardo Garre <eduardo.garre@gmail.com>
2021-10-03 10:38:01 +0200

    Elimino 2 funciones de la biblioteca estándar

--- biblioteca/inicio.c
+++ biblioteca/inicio.c
@@ -3,18 +3,6 @@ extern void __security_init_cookie(void);
 
 #include "es.h"
 
-extern int hola()
-{
-    txt texto = creaTxt("hola, ");
-    return escribe(texto);
-}
-
-extern int mundo()
-{
-    txt texto = creaTxt("mundo\n");
-    return escribe(texto);
-}
-
 extern int holamundo()
 {
     txt texto = sumaTxt(creaTxt("hola, "), creaTxt("mundo!\n"));
